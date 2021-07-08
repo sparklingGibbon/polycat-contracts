c41331fe745618e028413c3b844d7fcf0933053b
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-07-08 11:32:06 -0500

    Update StrategyMasterchef.sol

--- Vault2/StrategyMasterchef.sol
+++ Vault2/StrategyMasterchef.sol
@@ -1,113 +1,91 @@
 // SPDX-License-Identifier: MIT
 
+/*
+Join us at PolyCrystal.Finance!
+█▀▀█ █▀▀█ █░░ █░░█ █▀▀ █▀▀█ █░░█ █▀▀ ▀▀█▀▀ █▀▀█ █░░ 
+█░░█ █░░█ █░░ █▄▄█ █░░ █▄▄▀ █▄▄█ ▀▀█ ░░█░░ █▄▄█ █░░ 
+█▀▀▀ ▀▀▀▀ ▀▀▀ ▄▄▄█ ▀▀▀ ▀░▀▀ ▄▄▄█ ▀▀▀ ░░▀░░ ▀░░▀ ▀▀▀
+*/
+
 pragma solidity 0.6.12;
 
-import "./libs/IMasterchef.sol";
+import "./interfaces/IMasterHealer.sol";
 
-import "./BaseStrategyLPSingle.sol";
+import "./BaseStrategyApeLPSingle.sol";
 
-contract StrategyMasterchef is BaseStrategyLPSingle {
+contract StrategyMasterHealer is BaseStrategyApeLPSingle {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    address public masterchefAddress;
+    IMasterHealer public masterHealer;
     uint256 public pid;
 
     constructor(
-        address _vaultChefAddress,
-        address _masterchefAddress,
-        address _uniRouterAddress,
+        address _vaultHealerAddress,
+        address _masterHealerAddress,
+        address _apeFactoryAddress,
         uint256 _pid,
         address _wantAddress,
         address _earnedAddress,
-        address[] memory _earnedToWmaticPath,
-        address[] memory _earnedToUsdcPath,
-        address[] memory _earnedToFishPath,
+        uint256 _compoundMode,
+        address[] memory _earnedToCrystlPath,
         address[] memory _earnedToToken0Path,
-        address[] memory _earnedToToken1Path,
-        address[] memory _token0ToEarnedPath,
-        address[] memory _token1ToEarnedPath
+        address[] memory _earnedToToken1Path
     ) public {
         govAddress = msg.sender;
-        vaultChefAddress = _vaultChefAddress;
-        masterchefAddress = _masterchefAddress;
-        uniRouterAddress = _uniRouterAddress;
+        vaultHealerAddress = _vaultHealerAddress;
+        masterHealer = IMasterHealer(_masterHealerAddress);
+        apeFactoryAddress = _apeFactoryAddress;
 
         wantAddress = _wantAddress;
-        token0Address = IUniPair(wantAddress).token0();
-        token1Address = IUniPair(wantAddress).token1();
+        token0Address = IApePair(wantAddress).token0();
+        token1Address = IApePair(wantAddress).token1();
 
         pid = _pid;
         earnedAddress = _earnedAddress;
+    
+        compoundMode = _compoundMode;
 
-        earnedToWmaticPath = _earnedToWmaticPath;
-        earnedToUsdcPath = _earnedToUsdcPath;
-        earnedToFishPath = _earnedToFishPath;
+        earnedToCrystlPath = _earnedToCrystlPath;
         earnedToToken0Path = _earnedToToken0Path;
         earnedToToken1Path = _earnedToToken1Path;
-        token0ToEarnedPath = _token0ToEarnedPath;
-        token1ToEarnedPath = _token1ToEarnedPath;
 
-        transferOwnership(vaultChefAddress);
+        transferOwnership(vaultHealerAddress);
         
         _resetAllowances();
     }
 
     function _vaultDeposit(uint256 _amount) internal override {
-        IMasterchef(masterchefAddress).deposit(pid, _amount);
+        masterHealer.deposit(pid, _amount);
     }
     
     function _vaultWithdraw(uint256 _amount) internal override {
-        IMasterchef(masterchefAddress).withdraw(pid, _amount);
+        masterHealer.withdraw(pid, _amount);
     }
     
     function _vaultHarvest() internal override {
-        IMasterchef(masterchefAddress).withdraw(pid, 0);
+        masterHealer.withdraw(pid, 0);
     }
     
     function vaultSharesTotal() public override view returns (uint256) {
-        (uint256 amount,) = IMasterchef(masterchefAddress).userInfo(pid, address(this));
+        (uint256 amount,) = masterHealer.userInfo(pid, address(this));
         return amount;
     }
-    
+     
     function wantLockedTotal() public override view returns (uint256) {
         return IERC20(wantAddress).balanceOf(address(this))
             .add(vaultSharesTotal());
     }
 
     function _resetAllowances() internal override {
-        IERC20(wantAddress).safeApprove(masterchefAddress, uint256(0));
+        IERC20(wantAddress).safeApprove(address(masterHealer), uint256(0));
         IERC20(wantAddress).safeIncreaseAllowance(
-            masterchefAddress,
-            uint256(-1)
-        );
-
-        IERC20(earnedAddress).safeApprove(uniRouterAddress, uint256(0));
-        IERC20(earnedAddress).safeIncreaseAllowance(
-            uniRouterAddress,
-            uint256(-1)
-        );
-
-        IERC20(token0Address).safeApprove(uniRouterAddress, uint256(0));
-        IERC20(token0Address).safeIncreaseAllowance(
-            uniRouterAddress,
-            uint256(-1)
-        );
-
-        IERC20(token1Address).safeApprove(uniRouterAddress, uint256(0));
-        IERC20(token1Address).safeIncreaseAllowance(
-            uniRouterAddress,
-            uint256(-1)
-        );
-
-        IERC20(usdcAddress).safeApprove(rewardAddress, uint256(0));
-        IERC20(usdcAddress).safeIncreaseAllowance(
-            rewardAddress,
+            address(masterHealer),
             uint256(-1)
         );
     }
     
     function _emergencyVaultWithdraw() internal override {
-        IMasterchef(masterchefAddress).emergencyWithdraw(pid);
+        masterHealer.emergencyWithdraw(pid);
     }
 }
