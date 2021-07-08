86d4d396d2092357b8a56750262907737e43d4f3
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-07-08 11:26:12 -0500

    Update BaseStrategy.sol

--- Vault2/BaseStrategy.sol
+++ Vault2/BaseStrategy.sol
@@ -1,4 +1,12 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT (routing code is GPL from apeswap and needs to be dealt with)
+
+/*
+Join us at PolyCrystal.Finance!
+█▀▀█ █▀▀█ █░░ █░░█ █▀▀ █▀▀█ █░░█ █▀▀ ▀▀█▀▀ █▀▀█ █░░ 
+█░░█ █░░█ █░░ █▄▄█ █░░ █▄▄▀ █▄▄█ ▀▀█ ░░█░░ █▄▄█ █░░ 
+█▀▀▀ ▀▀▀▀ ▀▀▀ ▄▄▄█ ▀▀▀ ▀░▀▀ ▄▄▄█ ▀▀▀ ░░▀░░ ▀░░▀ ▀▀▀
+*/
+
 
 pragma solidity 0.6.12;
 
@@ -7,11 +15,10 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/utils/Pausable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/utils/ReentrancyGuard.sol";
 
-import "./libs/IStrategyFish.sol";
-import "./libs/IUniPair.sol";
-import "./libs/IUniRouter02.sol";
+import "./interfaces/IApePair.sol";
+import { ApeLibrary } from "./libraries/ApeLibrary.sol";
 
-abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
+abstract contract BaseStrategy is Ownable, Pausable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
@@ -20,35 +27,30 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
     address public token1Address;
     address public earnedAddress;
     
-    address public uniRouterAddress;
-    address public constant usdcAddress = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-    address public constant fishAddress = 0x3a3Df212b7AA91Aa0402B9035b098891d276572B;
-    address public constant rewardAddress = 0x917FB15E8aAA12264DCBdC15AFef7cD3cE76BA39;
-    address public constant withdrawFeeAddress = 0x4879712c5D1A98C0B88Fb700daFF5c65d12Fd729;
-    address public constant feeAddress = 0x1cb757f1eB92F25A917CE9a92ED88c1aC0734334;
-    address public vaultChefAddress;
+    address public apeFactoryAddress = 0xCf083Be4164828f00cAE704EC15a36D711491284;
+    address public constant crystlAddress = 0x76bF0C28e604CC3fE9967c83b3C3F31c213cfE64;
+    address public constant rewardAddress = 0x917FB15E8aAA12264DCBdC15AFef7cD3cE76BA39; //UPDATE
+    address public constant withdrawFeeAddress = 0x4879712c5D1A98C0B88Fb700daFF5c65d12Fd729; //UPDATE
+    address public constant feeAddress = 0x1cb757f1eB92F25A917CE9a92ED88c1aC0734334; //UPDATE
+    address public vaultHealerAddress;
     address public govAddress;
 
     uint256 public lastEarnBlock = block.number;
     uint256 public sharesTotal = 0;
 
+    uint256 public compoundMode;
     address public constant buyBackAddress = 0x000000000000000000000000000000000000dEaD;
-    uint256 public controllerFee = 50;
-    uint256 public rewardRate = 0;
-    uint256 public buyBackRate = 450;
-    uint256 public constant feeMaxTotal = 1000;
+    uint256 public controllerFee = 0;
+    uint256 public buyBackRate = 200;
+    uint256 public constant feeMaxTotal = 500;
     uint256 public constant feeMax = 10000; // 100 = 1%
 
-    uint256 public withdrawFeeFactor = 10000; // 0% withdraw fee
-    uint256 public constant withdrawFeeFactorMax = 10000;
-    uint256 public constant withdrawFeeFactorLL = 9900;
-
-    uint256 public slippageFactor = 950; // 5% default slippage tolerance
-    uint256 public constant slippageFactorUL = 995;
+    uint256 public withdrawFeeFactor = 10; // 0.10% withdraw fee
+    uint256 public constant withdrawFeeFactorMax = 500;
 
     address[] public earnedToWmaticPath;
     address[] public earnedToUsdcPath;
-    address[] public earnedToFishPath;
+    address[] public earnedToCrystlPath;
     address[] public earnedToToken0Path;
     address[] public earnedToToken1Path;
     address[] public token0ToEarnedPath;
@@ -56,11 +58,8 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
     
     event SetSettings(
         uint256 _controllerFee,
-        uint256 _rewardRate,
         uint256 _buyBackRate,
-        uint256 _withdrawFeeFactor,
-        uint256 _slippageFactor,
-        address _uniRouterAddress
+        uint256 _withdrawFeeFactor
     );
     
     modifier onlyGov() {
@@ -70,13 +69,30 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
 
     function _vaultDeposit(uint256 _amount) internal virtual;
     function _vaultWithdraw(uint256 _amount) internal virtual;
-    function earn() external virtual;
     function vaultSharesTotal() public virtual view returns (uint256);
     function wantLockedTotal() public virtual view returns (uint256);
     function _resetAllowances() internal virtual;
     function _emergencyVaultWithdraw() internal virtual;
+    function _compound() internal virtual;
     
-    function deposit(address _userAddress, uint256 _wantAmt) external onlyOwner nonReentrant whenNotPaused returns (uint256) {
+    //0: compound by anyone; 1: EOA only; 2: gov only
+    modifier compounding() {
+        if (!paused() && (compoundMode == 0 || msg.sender == govAddress || (compoundMode == 1 && msg.sender == tx.origin)))
+            _compound();
+        _;
+    }
+    function compound() external whenNotPaused {
+        require(compoundMode == 0 || msg.sender == govAddress || (compoundMode == 1 && msg.sender == tx.origin),
+            "Compounding disabled"
+        );
+        _compound();
+    }
+    function setCompoundMode(uint mode) external virtual onlyGov {
+        
+        compoundMode = mode;
+    }
+    
+    function deposit(address _userAddress, uint256 _wantAmt) external onlyOwner whenNotPaused compounding returns (uint256) {
         // Call must happen before transfer
         uint256 wantLockedBefore = wantLockedTotal();
 
@@ -107,7 +123,8 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
         return sharesAfter.sub(sharesBefore);
     }
 
-    function withdraw(address _userAddress, uint256 _wantAmt) external onlyOwner nonReentrant returns (uint256) {
+    function withdraw(address _userAddress, uint256 _wantAmt) external onlyOwner compounding returns (uint256) {
+        
         require(_wantAmt > 0, "_wantAmt is 0");
         
         uint256 wantAmt = IERC20(wantAddress).balanceOf(address(this));
@@ -134,25 +151,28 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
         
         // Withdraw fee
         uint256 withdrawFee = _wantAmt
-            .mul(withdrawFeeFactorMax.sub(withdrawFeeFactor))
-            .div(withdrawFeeFactorMax);
+            .mul(withdrawFeeFactor)
+            / 10000;
         if (withdrawFee > 0) {
             IERC20(wantAddress).safeTransfer(withdrawFeeAddress, withdrawFee);
         }
         
         _wantAmt = _wantAmt.sub(withdrawFee);
 
-        IERC20(wantAddress).safeTransfer(vaultChefAddress, _wantAmt);
+        IERC20(wantAddress).safeTransfer(vaultHealerAddress, _wantAmt);
 
         return sharesRemoved;
     }
 
     // To pay for earn function
     function distributeFees(uint256 _earnedAmt) internal returns (uint256) {
+        
+        if (_earnedAmt == 0) return 0;
+        
         if (controllerFee > 0) {
-            uint256 fee = _earnedAmt.mul(controllerFee).div(feeMax);
+            uint256 fee = _earnedAmt.mul(controllerFee)/feeMax;
     
-            _safeSwapWmatic(
+            _swap(
                 fee,
                 earnedToWmaticPath,
                 feeAddress
@@ -164,35 +184,16 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
         return _earnedAmt;
     }
 
-    function distributeRewards(uint256 _earnedAmt) internal returns (uint256) {
-        if (rewardRate > 0) {
-            uint256 fee = _earnedAmt.mul(rewardRate).div(feeMax);
-    
-            uint256 usdcBefore = IERC20(usdcAddress).balanceOf(address(this));
-            
-            _safeSwap(
-                fee,
-                earnedToUsdcPath,
-                address(this)
-            );
-            
-            uint256 usdcAfter = IERC20(usdcAddress).balanceOf(address(this)).sub(usdcBefore);
-            
-            IStrategyFish(rewardAddress).depositReward(usdcAfter);
-            
-            _earnedAmt = _earnedAmt.sub(fee);
-        }
-
-        return _earnedAmt;
-    }
-
     function buyBack(uint256 _earnedAmt) internal virtual returns (uint256) {
+        
+        if (_earnedAmt == 0) return 0;
+        
         if (buyBackRate > 0) {
-            uint256 buyBackAmt = _earnedAmt.mul(buyBackRate).div(feeMax);
+            uint256 buyBackAmt = _earnedAmt.mul(buyBackRate)/feeMax;
     
-            _safeSwap(
+            _swap(
                 buyBackAmt,
-                earnedToFishPath,
+                earnedToCrystlPath,
                 buyBackAddress
             );
 
@@ -231,64 +232,47 @@ abstract contract BaseStrategy is Ownable, ReentrancyGuard, Pausable {
     
     function setSettings(
         uint256 _controllerFee,
-        uint256 _rewardRate,
         uint256 _buyBackRate,
-        uint256 _withdrawFeeFactor,
-        uint256 _slippageFactor,
-        address _uniRouterAddress
+        uint256 _withdrawFeeFactor
     ) external onlyGov {
-        require(_controllerFee.add(_rewardRate).add(_buyBackRate) <= feeMaxTotal, "Max fee of 10%");
-        require(_withdrawFeeFactor >= withdrawFeeFactorLL, "_withdrawFeeFactor too low");
+        require(_controllerFee.add(_buyBackRate) <= feeMaxTotal, "Max fee of 5%");
         require(_withdrawFeeFactor <= withdrawFeeFactorMax, "_withdrawFeeFactor too high");
-        require(_slippageFactor <= slippageFactorUL, "_slippageFactor too high");
         controllerFee = _controllerFee;
-        rewardRate = _rewardRate;
         buyBackRate = _buyBackRate;
         withdrawFeeFactor = _withdrawFeeFactor;
-        slippageFactor = _slippageFactor;
-        uniRouterAddress = _uniRouterAddress;
 
         emit SetSettings(
             _controllerFee,
-            _rewardRate,
             _buyBackRate,
-            _withdrawFeeFactor,
-            _slippageFactor,
-            _uniRouterAddress
+            _withdrawFeeFactor
         );
     }
-    
-    function _safeSwap(
-        uint256 _amountIn,
-        address[] memory _path,
-        address _to
-    ) internal {
-        uint256[] memory amounts = IUniRouter02(uniRouterAddress).getAmountsOut(_amountIn, _path);
-        uint256 amountOut = amounts[amounts.length.sub(1)];
 
-        IUniRouter02(uniRouterAddress).swapExactTokensForTokens(
-            _amountIn,
-            amountOut.mul(slippageFactor).div(1000),
-            _path,
-            _to,
-            now.add(600)
-        );
-    }
     
-    function _safeSwapWmatic(
-        uint256 _amountIn,
-        address[] memory _path,
-        address _to
+    //optimized swap routing as slippage and deadlines aren't a factor
+    function _swap(
+        uint amountIn,
+        address[] memory path,
+        address recipient
     ) internal {
-        uint256[] memory amounts = IUniRouter02(uniRouterAddress).getAmountsOut(_amountIn, _path);
-        uint256 amountOut = amounts[amounts.length.sub(1)];
-
-        IUniRouter02(uniRouterAddress).swapExactTokensForETH(
-            _amountIn,
-            amountOut.mul(slippageFactor).div(1000),
-            _path,
-            _to,
-            now.add(600)
-        );
+        
+        //eg, the common case of earn crystl, "buyback" crystl
+        if (path.length == 1) {
+            IERC20(path[0]).safeTransfer(recipient, amountIn);
+            return;
+        }
+        
+        uint[] memory amounts = ApeLibrary.getAmountsOut(apeFactoryAddress, amountIn, path);
+        
+        IERC20(path[0]).safeTransfer(ApeLibrary.pairFor(apeFactoryAddress, path[0], path[1]), amounts[0]);
+            
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (uint amount0Out, uint amount1Out) = input < output ? (uint(0), amounts[i + 1]) : (amounts[i + 1], uint(0));
+            address to = i < path.length - 2 ? ApeLibrary.pairFor(apeFactoryAddress, output, path[i + 2]) : recipient;
+            IApePair(ApeLibrary.pairFor(apeFactoryAddress, input, output)).swap(
+                amount0Out, amount1Out, to, new bytes(0)
+            );
+        }
     }
 }
