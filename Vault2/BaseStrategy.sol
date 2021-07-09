b8f15be8feb6e7861337ae20f710f012427de2d6
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-07-08 19:30:49 -0500

    Update BaseStrategy.sol

--- Vault2/BaseStrategy.sol
+++ Vault2/BaseStrategy.sol
@@ -28,11 +28,7 @@ abstract contract BaseStrategy is Ownable, Pausable {
     address public token1Address;
     address public earnedAddress;
     
-    address public apeFactoryAddress = 0xCf083Be4164828f00cAE704EC15a36D711491284;
     address public constant crystlAddress = 0x76bF0C28e604CC3fE9967c83b3C3F31c213cfE64;
-    address public constant rewardAddress = 0x917FB15E8aAA12264DCBdC15AFef7cD3cE76BA39; //UPDATE
-    address public constant withdrawFeeAddress = 0x4879712c5D1A98C0B88Fb700daFF5c65d12Fd729; //UPDATE
-    address public constant feeAddress = 0x1cb757f1eB92F25A917CE9a92ED88c1aC0734334; //UPDATE
     address public vaultHealerAddress;
     address public govAddress;
 
@@ -40,16 +36,15 @@ abstract contract BaseStrategy is Ownable, Pausable {
     uint256 public sharesTotal = 0;
 
     address public constant buyBackAddress = 0x000000000000000000000000000000000000dEaD;
-    uint256 public controllerFee = 0;
+
     uint256 public buyBackRate = 200;
     uint256 public constant feeMaxTotal = 500;
     uint256 public constant feeMax = 10000; // 100 = 1%
 
-    uint256 public withdrawFeeFactor = 10; // 0.10% withdraw fee
+    uint256 public withdrawFeeFactor = 0;
     uint256 public constant withdrawFeeFactorMax = 500;
 
     address[] public earnedToWmaticPath;
-    address[] public earnedToUsdcPath;
     address[] public earnedToCrystlPath;
     address[] public earnedToToken0Path;
     address[] public earnedToToken1Path;
@@ -57,7 +52,6 @@ abstract contract BaseStrategy is Ownable, Pausable {
     address[] public token1ToEarnedPath;
     
     event SetSettings(
-        uint256 _controllerFee,
         uint256 _buyBackRate,
         uint256 _withdrawFeeFactor
     );
@@ -137,7 +131,11 @@ abstract contract BaseStrategy is Ownable, Pausable {
             .mul(withdrawFeeFactor)
             / 10000;
         if (withdrawFee > 0) {
-            IERC20(wantAddress).safeTransfer(withdrawFeeAddress, withdrawFee);
+            GibbonRouter._swap(
+                withdrawFee,
+                earnedToCrystlPath,
+                buyBackAddress
+            );
         }
         
         _wantAmt = _wantAmt.sub(withdrawFee);
@@ -145,26 +143,6 @@ abstract contract BaseStrategy is Ownable, Pausable {
         IERC20(wantAddress).safeTransfer(vaultHealerAddress, _wantAmt);
 
         return sharesRemoved;
-    }
-
-    // To pay for earn function
-    function distributeFees(uint256 _earnedAmt) internal returns (uint256) {
-        
-        if (_earnedAmt == 0) return 0;
-        
-        if (controllerFee > 0) {
-            uint256 fee = _earnedAmt.mul(controllerFee)/feeMax;
-    
-            GibbonRouter._swap(
-                fee,
-                earnedToWmaticPath,
-                feeAddress
-            );
-            
-            _earnedAmt = _earnedAmt.sub(fee);
-        }
-
-        return _earnedAmt;
     }
 
     function buyBack(uint256 _earnedAmt) internal virtual returns (uint256) {
@@ -214,18 +192,15 @@ abstract contract BaseStrategy is Ownable, Pausable {
     }
     
     function setSettings(
-        uint256 _controllerFee,
         uint256 _buyBackRate,
         uint256 _withdrawFeeFactor
     ) external onlyGov {
-        require(_controllerFee.add(_buyBackRate) <= feeMaxTotal, "Max fee of 5%");
+        require(_buyBackRate <= feeMaxTotal, "Max fee of 5%");
         require(_withdrawFeeFactor <= withdrawFeeFactorMax, "_withdrawFeeFactor too high");
-        controllerFee = _controllerFee;
         buyBackRate = _buyBackRate;
         withdrawFeeFactor = _withdrawFeeFactor;
 
         emit SetSettings(
-            _controllerFee,
             _buyBackRate,
             _withdrawFeeFactor
         );
